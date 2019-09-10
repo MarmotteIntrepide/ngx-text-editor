fdd7600ce1104956a7c412be776b5cef7c1ccbc5
Jeremy Quick <jrquick@uab.edu>
2019-09-10 11:01:51 -0500

    Spacing

--- src/app/ngx-text-editor/ngx-text-editor.module.ts
+++ src/app/ngx-text-editor/ngx-text-editor.module.ts
@@ -1,20 +1,35 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
-import { PopoverModule } from 'ngx-bootstrap';
-import { NgxTextEditorComponent } from './ngx-text-editor.component';
-import { NgxGrippieComponent } from './ngx-grippie/ngx-grippie.component';
-import { NgxTextEditorMessageComponent } from './ngx-text-editor-message/ngx-text-editor-message.component';
-import { NgxTextEditorToolbarComponent } from './ngx-text-editor-toolbar/ngx-text-editor-toolbar.component';
-import { MessageService } from './common/services/message.service';
-import { CommandExecutorService } from './common/services/command-executor.service';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
+import {PopoverModule} from 'ngx-bootstrap';
+import {NgxTextEditorComponent} from './ngx-text-editor.component';
+import {NgxGrippieComponent} from './ngx-grippie/ngx-grippie.component';
+import {NgxTextEditorMessageComponent} from './ngx-text-editor-message/ngx-text-editor-message.component';
+import {NgxTextEditorToolbarComponent} from './ngx-text-editor-toolbar/ngx-text-editor-toolbar.component';
+import {MessageService} from './common/services/message.service';
+import {CommandExecutorService} from './common/services/command-executor.service';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, PopoverModule.forRoot()],
-  declarations: [NgxTextEditorComponent, NgxGrippieComponent, NgxTextEditorMessageComponent, NgxTextEditorToolbarComponent],
-  exports: [NgxTextEditorComponent],
-  providers: [CommandExecutorService, MessageService]
+  declarations: [
+    NgxTextEditorComponent,
+    NgxGrippieComponent,
+    NgxTextEditorMessageComponent,
+    NgxTextEditorToolbarComponent
+  ],
+  exports: [
+    NgxTextEditorComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    PopoverModule.forRoot(),
+    ReactiveFormsModule
+  ],
+  providers: [
+    CommandExecutorService,
+    MessageService
+  ]
 })
 
 export class NgxTextEditorModule { }
